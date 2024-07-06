a1209e5f4bbc57790a142ef25b6041f63d95ea71
Isaac Machakata <isaac@caasi.co.zw>
2024-07-06 08:32:49 +0200

    chore: test instantiating with invalid values

--- tests/BulkSmsApiTest.php
+++ tests/BulkSmsApiTest.php
@@ -12,11 +12,26 @@ class BulkSmsApiTest extends TestCase
     {
         $this->assertIsObject(new BulkSmsApi($config));
     }
+    #[DataProvider('setEmptyConfigValues')]
+    public function testEmptyConfigurationsThrowsErrors($config)
+    {
+        $this->expectException(BulkSmsException::class);
+        new BulkSmsApi($config);
+    }
     public static function setConfigValues(): array
     {
         return [
             ['a-valid-api-token-key'],
             [array('username' => 'admin', 'password' => 'supersecret')],
+        ];
+    }
+    public static function setEmptyConfigValues(): array
+    {
+        return [
+            [''],
+            [array()],
+            [array('username' => 'admin')],
+            [array('username' => 'supersecret')],
         ];
     }
 }
