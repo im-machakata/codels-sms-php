b84abc2c4ea1187a2bfcc76b820d5e9f0a67cc4c
Isaac Machakata <isaac@caasi.co.zw>
2024-07-06 08:32:16 +0200

    chore: test instantiating with valida configurations

---
+++ tests/BulkSmsApiTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use IsaacMachakata\CodelSms\BulkSmsApi;
+use IsaacMachakata\CodelSms\BulkSmsException;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+class BulkSmsApiTest extends TestCase
+{
+    #[DataProvider('setConfigValues')]
+    public function testValidConfigurations($config)
+    {
+        $this->assertIsObject(new BulkSmsApi($config));
+    }
+    public static function setConfigValues(): array
+    {
+        return [
+            ['a-valid-api-token-key'],
+            [array('username' => 'admin', 'password' => 'supersecret')],
+        ];
+    }
+}
