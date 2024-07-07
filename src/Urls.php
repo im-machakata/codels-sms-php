d8dc8ec5902e393879dbded748471395b4ddfac2
Isaac Machakata <isaac@caasi.co.zw>
2024-07-07 04:17:16 +0200

    refactor: add urls constant

---
+++ src/Urls.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace IsaacMachakata\CodelSms;
+
+/**
+ * Constant URLS for the API
+ */
+class Urls
+{
+    /**
+     * The endpoint url for bulk sms
+     */
+    const BASE_URL = 'https://2waychat.com/2wc';
+    const SINGLE_SMS_ENDPOINT = '/single-sms/v1/api';
+    const MULTIPLE_SMS_ENDPOINT = '/multiple-sms/v1/api';
+}
