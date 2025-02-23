820b45d24e4d750ac7226367dfd7c3a32be8ecc7
Isaac Machakata <isaac@caasi.co.zw>
2025-02-23 04:15:39 +0000

    refactor: send async request

--- src/Client.php
+++ src/Client.php
@@ -104,15 +104,13 @@ final class Client implements ClientInterface
 
         $requests = function ($total) use ($sms, $uri) {
             foreach ($sms as $message) {
-                yield function () use ($message, $uri) {
-                    return $this->client->requestAsync('POST', $uri, [
-                        // 'headers' => [],
-                        'json' => [
-                            ...$message->toArray(),
-                            'token' => $this->config,
-                        ],
-                    ]);
-                };
+                yield $this->client->requestAsync('POST', $uri, [
+                    // 'headers' => [],
+                    'json' => [
+                        ...$message->toArray(),
+                        'token' => $this->config,
+                    ],
+                ]);
             }
         };
 
